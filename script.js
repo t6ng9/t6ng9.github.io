28fe8f1f635cdf6fb233a65c39aa709007892c4f
t6ng9 <hardandfast96@gmail.com>
2024-10-22 22:14:14 +0900

    Initial commit - Wedding Invitation

--- script.js
+++
@@ -1,8 +0,0 @@
-function showMap() {
-    const map = document.getElementById('map');
-    if (map.style.display === 'none') {
-        map.style.display = 'block';
-    } else {
-        map.style.display = 'none';
-    }
-}
